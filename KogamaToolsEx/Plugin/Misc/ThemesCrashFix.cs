0b7f069551b6214840f539f26744f953ada845ee
bowse <bowse@DESKTOP-RIMJQ11>
2026-03-12 15:24:44 -0300

    Fix themes crash (hopefully)

---
+++ KogamaToolsEx/Plugin/Misc/ThemesCrashFix.cs
@@ -0,0 +1,32 @@
+﻿using HarmonyLib;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace KogamaToolsEx.Plugin.Misc
+{
+    [HarmonyPatch]
+    // shoutouts to Nightus!!
+    internal class ThemesCrashFix
+    {
+        [HarmonyPatch(typeof(Theme), "Initialize", [typeof(int)])]
+        [HarmonyPrefix]
+        private static void ThemeInitializeIntPrefix(int woid, ref bool __runOriginal)
+        {
+            var wo = MVGameControllerBase.WOCM.GetWorldObject(woid) as MVWorldObjectClient;
+
+            if (wo != null)
+            {
+                __runOriginal = true;
+                return;
+            }
+
+            // todo: recover??? i'm pretty sure we could just request a new theme world object
+            // right??
+            wo.Destroy();
+            __runOriginal = false;
+        }
+    }
+}
