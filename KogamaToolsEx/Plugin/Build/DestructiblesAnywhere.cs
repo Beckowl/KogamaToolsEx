bbf536f2152c3842c3ad6f3834a6cda4d4d48642
bowse <bowse@DESKTOP-RIMJQ11>
2026-03-17 02:46:13 -0300

    Add no build limit & destructible materials anywhere

---
+++ KogamaToolsEx/Plugin/Build/DestructiblesAnywhere.cs
@@ -0,0 +1,16 @@
+﻿using HarmonyLib;
+
+namespace KogamaToolsEx.Plugin.Build
+{
+    // Lets you use destructible materials in any model
+    [HarmonyPatch]
+    internal static class DestructiblesAnywhere
+    {
+        [HarmonyPatch(typeof(MVMaterial), nameof(MVMaterial.IsAvailable), MethodType.Getter)]
+        [HarmonyPostfix]
+        private static void MVMaterials_get_IsAvailable(ref bool __result)
+        {
+            __result = true;
+        }
+    }
+}
---
+++ KogamaToolsEx/Plugin/Build/NoBuildLimit.cs
@@ -0,0 +1,26 @@
+﻿using HarmonyLib;
+
+namespace KogamaToolsEx.Plugin.Build
+{
+    [HarmonyPatch]
+    internal static class NoBuildLimit
+    {
+        // TODO: Constrain avatars
+
+        [HarmonyPatch(typeof(ConstraintVisualizer), "Init")]
+        [HarmonyPrefix]
+        private static void ConstraintVisualizer_Init_Prefix(ref bool __runOriginal)
+        {
+            __runOriginal = false;
+        }
+
+        [HarmonyPatch(typeof(ModelingDynamicBoxConstraint), "CanAddCubeAt")]
+        [HarmonyPatch(typeof(ModelingBoxCountConstraint), "CanAddCubeAt")]
+        [HarmonyPatch(typeof(ModelingBoxCountConstraint), "CanRemoveCubeAt")]
+        [HarmonyPostfix]
+        private static void CanAddRemoveCubeAt_Postfix(ref bool __result)
+        {
+            __result = true;
+        }
+    }
+}
