92bc7b00ba0ea8337c57284aaf2b55af555d748b
bowse <bowse@DESKTOP-RIMJQ11>
2026-03-18 04:45:14 -0300

    add anti ban

---
+++ KogamaToolsEx/Plugin/Misc/AntiBan.cs
@@ -0,0 +1,22 @@
+﻿using HarmonyLib;
+using MV.Common;
+
+namespace KogamaToolsEx.Plugin.Misc
+{
+    [HarmonyPatch]
+    internal static class AntiBan
+    {
+        [HarmonyPatch(typeof(CheatHandling), "Init")]
+        [HarmonyPatch(typeof(CheatHandling), "ExecuteBan")]
+        [HarmonyPatch(typeof(CheatHandling), "MachineBanDetected")]
+        [HarmonyPatch(typeof(MVNetworkGame.OperationRequests), "Ban", [typeof(int), typeof(MVPlayer), typeof(string)])]
+        [HarmonyPatch(typeof(MVNetworkGame.OperationRequests), "Ban", [typeof(CheatType)])]
+        [HarmonyPatch(typeof(MVNetworkGame.OperationRequests), "Expel")]
+        [HarmonyPatch(typeof(MVNetworkGame.OperationRequests), "Kick")]
+        [HarmonyPrefix]
+        private static void NoBan(ref bool __runOriginal)
+        {
+            __runOriginal = false;
+        }
+    }
+}
